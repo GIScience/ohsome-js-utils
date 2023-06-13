bd05dfc906ec8371829109a39625162f3dd9dcf5
Michael Auer <michael.auer@heigit.org>
2023-06-13 16:19:09 +0200

    build: simplify build process and package structure

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
-import { defineConfig } from 'rollup';
+import {defineConfig} from 'rollup';
 import terser from '@rollup/plugin-terser';
-import generatePackageJson from 'rollup-plugin-generate-package-json';
-import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   input: 'src/index.ts',
@@ -24,34 +22,6 @@ export default defineConfig({
     }
   ],
   plugins: [
-    typescript(),
-    generatePackageJson({
-      inputFolder: '.',
-      outputFolder: 'dist',
-      baseContents: (packageJson) => {
-        delete packageJson.scripts;
-        delete packageJson.devDependencies;
-        packageJson.exports = {
-          './package.json': {
-            'default': './package.json'
-          },
-          '.': {
-            'types': './index.d.ts',
-            'es2020': './ohsome-js-utils.mjs',
-            'node': './ohsome-js-utils.umd.min.js',
-            'default': './ohsome-js-utils.umd.min.js'
-          }
-        };
-        packageJson.sideEffects = false;
-
-        return packageJson;
-      }
-    }),
-    copy({
-      targets: [
-        { src: 'README.md', dest: 'dist',
-          transform: (contents, filename) => contents.toString().replace('dist/', './') }
-      ]
-    })
+    typescript()
   ]
 });
